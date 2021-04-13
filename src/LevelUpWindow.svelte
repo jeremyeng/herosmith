48f199787b5fbb1ddd07d32f1ca478e6c676db78
Jeremy Eng <jeremy@jeremyeng.com>
2021-04-13 19:24:52 -0400

    implement simple level up window

---
+++ src/LevelUpWindow.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let name;
+
+	let tabs = ["Features", "Spells", "Review"];
+
+	let currentTab = "Features";
+	$: console.log(`Herosmith | Current Tab changed to ${currentTab}`);
+	
+	function handleClick(event) {
+		currentTab = event.target.text;
+	}
+</script>
+
+<form>
+	<nav data-group="">
+		{#each tabs as tab}
+    	<!-- svelte-ignore a11y-missing-attribute -->
+    	<a class:active={currentTab === tab} on:click={handleClick}>{tab}</a>
+		{/each}
+  </nav>
+</form>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-evenly;
+		font-family: "Modesto Condensed", "Palatino Linotype", serif;
+		font-size: 20px;
+    font-weight: 700;
+	}
+
+	a:hover {
+		text-shadow: 0 0 10px red;
+	}
+
+	.active {
+		border-bottom: 3px solid #44191A;
+		text-shadow: 0 0 10px red;
+	}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import App from './App.svelte';
+import LevelUpWindow from './LevelUpWindow.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+class MyApp extends Application {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      template: `modules/herosmith/templates/level-up-window.html`,
+      tabs: [{navSelector: ".tabs", contentSelector: "form", initial: "features"}],
+      width: 600,
+      height: 600
+    });
+  }
+  
+  activateListeners(html) {
+    this.component = new LevelUpWindow({
+      target: html.get(0),
+      props: {
+        name: "Foundry",
+      },
+    });
+  }
+}
+
+Hooks.on("ready", () => {
+	console.log("Herosmith | LOADED");
+  const app = new MyApp();
+  app.render(true);
 });
-
-export default app;
