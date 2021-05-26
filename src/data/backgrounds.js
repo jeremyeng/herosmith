470b4f78c8a4b13a17065639e4a2fb911dd366ca
Jeremy Eng <jeremy@jeremyeng.com>
2021-05-26 15:28:41 -0400

    add backgrounds data file

---
+++ src/data/backgrounds.js
@@ -0,0 +1,18 @@
+import languageOptions from "data/languageOptions.js";
+
+const BACKGROUNDS = {
+  // Acolyte
+  "Compendium.herosmith.backgrounds.Kd5m2GgxkvFozggc": {
+    choices: {
+      1: languageOptions(),
+    },
+    data: {
+      1: {
+        skill_proficiencies: ["ins", "rel"],
+      },
+    },
+    personality: "Compendium.herosmith.background_characteristics.ZowmTS1xYNcd80lM",
+  },
+};
+
+export default BACKGROUNDS;
