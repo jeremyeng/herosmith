2d708b37f8299d6702c9e088984fae916f3ced24
Jeremy Eng <jeremy@jeremyeng.com>
2021-07-11 13:41:30 -0400

    move ClassTab to use ItemGrid

--- src/components/ClassTab.svelte
+++ src/components/ClassTab.svelte
@@ -2,47 +2,49 @@
   import CLASSES from "data/classes.js";
   import Choice from "components/Choice.svelte";
   import ItemCard from "components/ItemCard.svelte";
+  import ItemGrid from "components/ItemGrid.svelte";
   import { fade } from "svelte/transition";
   import { cubicInOut } from "svelte/easing";
 
   export let data = {};
   export let level = 1;
+
+  function onSelectClass(event) {
+    const classUuid = event.detail.uuid;
+    if (data.class.uuid === classUuid) {
+      data.class = {
+        uuid: "",
+        data: {},
+        decisionData: {},
+      };
+      data.classEquipment = {
+        data: {},
+        decisionData: {},
+      };
+      data.spells = {
+        data: {},
+        decisionData: {},
+      };
+    } else {
+      data.class = {
+        uuid: classUuid,
+        data: CLASSES[classUuid]["data"][level],
+        decisionData: {},
+      };
+    }
+  }
 </script>
 
 <div class="class-tab">
   <h2>Classes</h2>
-  <div class="classes">
-    {#each Object.keys(CLASSES) as classUuid}
-      <ItemCard
-        uuid={classUuid}
-        disabled={classUuid !== data.class.uuid && data.class.uuid.length}
-        selected={classUuid === data.class.uuid}
-        on:selected={() => {
-          if (data.class.uuid === classUuid) {
-            data.class = {
-              uuid: "",
-              data: {},
-              decisionData: {},
-            };
-            data.classEquipment = {
-              data: {},
-              decisionData: {},
-            };
-            data.spells = {
-              data: {},
-              decisionData: {},
-            };
-          } else {
-            data.class = {
-              uuid: classUuid,
-              data: CLASSES[classUuid]["data"][level],
-              decisionData: {},
-            };
-          }
-        }}
-      />
-    {/each}
-  </div>
+  <ItemGrid
+    uuids={Object.keys(CLASSES)}
+    selectedUuids={data.class.uuid ? [data.class.uuid] : []}
+    maxSelectable={1}
+    selectable={true}
+    showQuantities={false}
+    on:selected={onSelectClass}
+  />
 
   {#if CLASSES[data.class.uuid]?.["choices"]?.[level]}
     <div class="choices" transition:fade|local={{ duration: 200, easing: cubicInOut }}>
